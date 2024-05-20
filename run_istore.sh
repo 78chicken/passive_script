8b117784d4e54bb0f5c7473dd94693bceee57f8c
78chicken <165230596+78chicken@users.noreply.github.com>
2024-05-20 17:24:50 +0800

    Create run_istore.sh

---
+++ run_istore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 保存到/opt/daily_job/restart.sh
+export restart_path=/opt/daily_job
+curl -s -o $restart_path/restart.sh https://raw.githubusercontent.com/78chicken/passive_script/main/restart_istore.sh
+
+# 確保脚本是可執行的
+chmod +x $restart_path/restart.sh
+
+# 執行腳本
+$restart_path/restart.sh
