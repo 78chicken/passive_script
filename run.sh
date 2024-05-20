1a5023f5b06afd754e3f009a96767aec7809d0ac
78chicken <165230596+78chicken@users.noreply.github.com>
2024-05-20 16:22:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # 保存到/opt/daily_job/restart.sh
 export restart_path=/opt/daily_job
-curl -s -o $restart_path/restart.sh https://raw.githubusercontent.com/78chicken/78chicken.github.com/main/restart.sh
+curl -s -o $restart_path/restart.sh https://raw.githubusercontent.com/78chicken/passive_script/main/restart.sh
 
 # 確保脚本是可執行的
 chmod +x $restart_path/restart.sh
