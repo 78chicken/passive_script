b3eb4207895689f28204215ec9a8dea006dc5164
78chicken <165230596+78chicken@users.noreply.github.com>
2024-05-20 16:09:20 +0800

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,63 @@
+#1. GagaNode
+sudo podman container stop GagaNode
+sleep 30s
+sudo podman container start GagaNode
+
+#2. Traffmonetizer
+sudo podman container stop Traffmonetizer
+sleep 30s
+sudo podman container start Traffmonetizer
+
+#3. Repocket
+sudo podman container stop Repocket
+sleep 30s
+sudo podman container start Repocket
+
+#4. Honeygain
+sudo podman container stop HoneyGain
+sleep 120s
+sudo podman container start HoneyGain
+
+#5. PacketStream
+sudo podman container stop PacketStream
+sleep 180s
+sudo podman container start PacketStream
+
+#6. Bitping
+sudo podman container stop Bitping
+sleep 30s
+sudo podman container start Bitping
+
+#7. EarnApp
+sudo podman container stop EarnApp
+sleep 30s
+sudo podman container start EarnApp
+
+#8. EarnFm
+sudo podman container stop EarnFm
+sleep 30s
+sudo podman container start EarnFm
+
+#9. IPRoyal
+sudo podman container stop IPRoyal
+sleep 90s
+sudo podman container start IPRoyal
+
+#10. SpeedShare
+sudo podman container stop SpeedShare
+sleep 30s
+sudo podman container start SpeedShare
+
+#11. Grass
+sudo podman container stop Grass
+sleep 30s
+sudo podman container start Grass
+
+#12. ProxyRack
+sudo podman container stop ProxyRack
+sleep 30s
+sudo podman container start ProxyRack
+
+#restart
+#sleep 10s
+#sudo reboot
