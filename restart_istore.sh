f4aa7ae423afbf1833e08ae8ae95bc6427ee126f
78chicken <165230596+78chicken@users.noreply.github.com>
2024-05-20 17:23:21 +0800

    Create restart_istore.sh

---
+++ restart_istore.sh
@@ -0,0 +1,64 @@
+#1. GagaNode
+#docker container stop GagaNode
+#sleep 30s
+#docker container start GagaNode
+
+#2. Traffmonetizer
+docker container stop Traffmonetizer
+sleep 30s
+docker container start Traffmonetizer
+
+#3. Repocket
+docker container stop Repocket
+sleep 30s
+docker container start Repocket
+
+#4. Honeygain
+docker container stop HoneyGain
+sleep 120s
+docker container start HoneyGain
+
+#5. PacketStream
+docker container stop PacketStream
+sleep 30s
+docker container start PacketStream
+
+#6. Bitping
+docker container stop Bitping
+sleep 30s
+docker container start Bitping
+
+#7. EarnApp
+docker container stop EarnApp
+sleep 30s
+docker container start EarnApp
+
+#8. EarnFm
+docker container stop EarnFm
+sleep 30s
+docker container start EarnFm
+
+#9. IPRoyal
+docker container stop IPRoyal
+sleep 90s
+docker container start IPRoyal
+
+#10. SpeedShare
+docker container stop SpeedShare
+sleep 30s
+docker container start SpeedShare
+
+#11. Grass
+docker container stop Grass
+sleep 30s
+docker container start Grass
+
+#12. ProxyLite
+docker container stop ProxyLite
+sleep 30s
+docker container start ProxyLite
+
+#13. ProxyRack
+docker container stop ProxyRack
+sleep 30s
+docker container start ProxyRack
