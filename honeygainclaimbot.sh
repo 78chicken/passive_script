aebb8eeb7c46b94003186cb04bedbe594ce97bd5
78chicken <165230596+78chicken@users.noreply.github.com>
2024-06-26 10:20:03 +0800

    Create honeygainclaimbot.sh

---
+++ honeygainclaimbot.sh
@@ -0,0 +1,38 @@
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=jyhfengli@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=jyhfeng0209@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=d8207491@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=jrfong96@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=mijong0603@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=ppan34717@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=wutiffany0403@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
+sleep 20s
+
+sudo podman run -d --rm --name bot -m 256m -e EMAIL=yamadali0209@gmail.com -e PASSWORD=q511612q xterna/honeygain-pot
+sleep 30s
+sudo podman stop bot
