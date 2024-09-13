dd41374ca83469ff28e7b392b068764cc531987f
78chicken <165230596+78chicken@users.noreply.github.com>
2024-09-13 11:01:15 +0800

    Update bearsharebot.sh

--- bearsharebot.sh
+++ bearsharebot.sh
@@ -1,7 +1,9 @@
 # jyhfengli@gmail.com
 sudo podman run -d --rm --name bot -m 400m -e EMAIL=jyhfengli@gmail.com -e PASSWORD=q511612q xterna/bearshare-pot
 sleep 60s
+sudo podman stop bot
 
 # yamadali0209@gmail.com
 sudo podman run -d --rm --name bot -m 400m -e EMAIL=yamadali0209@gmail.com -e PASSWORD=q511612q xterna/bearshare-pot
 sleep 60s
+sudo podman stop bot
