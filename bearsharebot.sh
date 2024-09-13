ccc86fd810a1f5c98f7c52b5a6e1e0d92d5024dd
78chicken <165230596+78chicken@users.noreply.github.com>
2024-09-13 10:17:34 +0800

    Create bearsharebot.sh

---
+++ bearsharebot.sh
@@ -0,0 +1,7 @@
+# jyhfengli@gmail.com
+sudo podman run -d --rm --name bot -m 400m -e EMAIL=jyhfengli@gmail.com -e PASSWORD=q511612q xterna/bearshare-pot
+sleep 60s
+
+# yamadali0209@gmail.com
+sudo podman run -d --rm --name bot -m 400m -e EMAIL=yamadali0209@gmail.com -e PASSWORD=q511612q xterna/bearshare-pot
+sleep 60s
