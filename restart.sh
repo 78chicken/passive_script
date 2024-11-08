ef45cbcedba3e92d9dbe079b1efc89327f6395da
78chicken <165230596+78chicken@users.noreply.github.com>
2024-11-08 23:37:30 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -75,7 +75,7 @@ sudo podman container start NodePay
 #14. BearShare
 sudo podman container stop BearShare
 sleep 30s
-#sudo podman container start BearShare
+sudo podman container start BearShare
 
 #restart
 #sleep 10s
