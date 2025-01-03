7bb8d844e966dd67820c3323ea47bd8eeb7ab69c
78chicken <165230596+78chicken@users.noreply.github.com>
2025-01-03 23:50:55 +0800

    Update install_portainer_ce.sh

--- install_portainer_ce.sh
+++ install_portainer_ce.sh
@@ -1,1 +1,1 @@
-sudo podman run --privileged -d -p 9000:9000 --name portainer --restart=always -v /run/podman/podman.sock -v portainer_data:/data portainer/portainer-ce
+sudo podman run --privileged -d -m 256m -p 9000:9000 --name portainer --restart=always -v /run/podman/podman.sock -v portainer_data:/data portainer/portainer-ce
