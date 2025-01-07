5eb99fb3a0c0b66eb0c2680e74865d5c08817aa9
78chicken <165230596+78chicken@users.noreply.github.com>
2025-01-07 23:46:27 +0800

    Update install_watchtower.sh

--- install_watchtower.sh
+++ install_watchtower.sh
@@ -1,1 +1,1 @@
-sudo podman run --privileged -d --restart --name watchtower -v /run/podman/podman.sock:/var/run/docker.sock containrrr/watchtower --cleanup --interval 21600
+sudo podman run --privileged -d --restart --name watchtower -v /run/podman/podman.sock:/var/run/docker.sock containrrr/watchtower --cleanup --remove-volumes --interval 21600
