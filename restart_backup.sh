ff3988cea9cd41cedb62b22af01819b386ab7ca3
78chicken <165230596+78chicken@users.noreply.github.com>
2025-02-20 15:31:41 +0800

    Create restart_backup.sh

---
+++ restart_backup.sh
@@ -0,0 +1,130 @@
+#1. Traffmonetizer
+sudo podman container stop Traffmonetizer
+sleep 30s
+sudo podman container start Traffmonetizer
+
+#2. Traffmonetizer2
+sudo podman container stop Traffmonetizer2
+sleep 30s
+sudo podman container start Traffmonetizer2
+
+#3. Repocket
+sudo podman container stop Repocket
+sleep 30s
+sudo podman container start Repocket
+
+#4. Repocket2
+sudo podman container stop Repocket2
+#sleep 30s
+#sudo podman container start Repocket2
+
+#5. Honeygain
+sudo podman container stop HoneyGain
+sleep 120s
+sudo podman container start HoneyGain
+
+#6. PacketStream
+sudo podman container stop PacketStream
+sleep 180s
+sudo podman container start PacketStream
+
+#7. EarnApp
+sudo podman container stop EarnApp
+sleep 30s
+sudo podman container start EarnApp
+
+#8. EarnFm
+sudo podman container stop EarnFm
+sleep 30s
+sudo podman container start EarnFm
+
+#9. IPRoyal
+sudo podman container stop IPRoyal
+sleep 90s
+sudo podman container start IPRoyal
+
+#10. Grass
+sudo podman container stop Grass
+sleep 30s
+sudo podman container start Grass
+
+#11. ProxyRack
+sudo podman container stop ProxyRack
+sleep 30s
+sudo podman container start ProxyRack
+
+#12. NodePay
+sudo podman container stop NodePay
+sleep 30s
+sudo podman container start NodePay
+
+#13. BlockMesh
+sudo podman container stop BlockMesh
+sleep 30s
+sudo podman container start BlockMesh
+
+#14 Titan
+sudo podman container stop Titan
+sleep 30s
+sudo podman container start Titan
+
+#15 OpenLoop
+sudo podman container stop OpenLoop
+sleep 15s
+sudo podman container start OpenLoop
+
+#16 Teneo
+sudo podman container stop Teneo
+#sleep 15s
+#sudo podman container start Teneo
+
+#17 Gaea
+sudo podman container stop Gaea
+sleep 15s
+sudo podman container start Gaea
+
+#18 DistributeAi
+sudo podman container stop DistributeAi
+sleep 15s
+sudo podman container start DistributeAi
+
+#19 DePINed
+sudo podman container stop DePINed
+sleep 15s
+sudo podman container start DePINed
+
+#20 Bless
+sudo podman container stop Bless
+sleep 15s
+sudo podman container start Bless
+
+#21 MinionLab
+#sudo podman container stop MinionLab
+#sleep 15s
+#sudo podman container start MinionLab
+
+#21 Unich
+sudo podman container stop Unich
+sleep 15s
+sudo podman container start Unich
+
+#21 Sparkchain
+sudo podman container stop Sparkchain
+sleep 15s
+sudo podman container start Sparkchain
+
+#21 Dawn
+sudo podman container stop Dawn
+sleep 15s
+sudo podman container start Dawn
+
+
+#100 刪除沒有tag的image
+sudo podman images -f "dangling=true" -q | xargs sudo podman rmi
+
+#101 刪除沒有使用的image
+sudo podman image prune -a -f
+
+#restart
+#sleep 10s
+#sudo reboot
