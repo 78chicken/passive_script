4470310c29261812a6bce0932ef8243b932687e8
78chicken <165230596+78chicken@users.noreply.github.com>
2025-02-24 11:21:41 +0800

    Update dailyjob.sh

--- dailyjob.sh
+++ dailyjob.sh
@@ -1,4 +1,4 @@
 echo "start to do HahaWallet job..."
 CID=$(sudo podman run -d --rm -m 50M -e TZ=Asia/Taipei --log-opt max-size=1m --log-opt max-file=1 --name HahaWallet -v /opt/build_image/hahawallet/accounts.json:/app/hahawallet/accounts.json 78chicken/hahawallet:latest)
-echo "HahaWallet CID is ${CID}"
+echo "HahaWallet CID is ${CID}, will be killed after 15s."
 sleep 15s && sudo podman stop ${CID}
