9d9d2ae3bd3c9ec695c02a24096925d647259f3e
78chicken <165230596+78chicken@users.noreply.github.com>
2025-02-24 11:03:43 +0800

    Update dailyjob.sh

--- dailyjob.sh
+++ dailyjob.sh
@@ -1,3 +1,3 @@
 echo "start to do Haha Wallet job..."
-CID=$(sudo podman run -d --rm -m 50M -e TZ=Asia/Taipei --log-opt max-size=1m --log-opt max-file=1  --name HahaWallet -v /opt/build_image/hahawallet/accounts.json:/app/hahawallet/accounts.json 78chicken/hahawallet:latest)
+CID=$(sudo podman run -d --rm -m 50M -e TZ=Asia/Taipei --log-opt max-size=1m --log-opt max-file=1 --name HahaWallet -v /opt/build_image/hahawallet/accounts.json:/app/hahawallet/accounts.json 78chicken/hahawallet:latest)
 sleep 15 && sudo podman stop $CID
