84bc55e69d359c7e9caa2d96a01ae5dfc849891c
78chicken <165230596+78chicken@users.noreply.github.com>
2025-02-26 17:35:33 +0800

    Update restart_backup.sh

--- restart_backup.sh
+++ restart_backup.sh
@@ -62,7 +62,7 @@ process_container() {
     local action_flag=$5
 
     if [[ "$update_flag" == "Y" ]]; then      
-        if [[ -n "$accounts_file" ]]; then
+        if [[ -n "$account" ]]; then
             echo "[$container_name] 下載 設定 ...$GITHUB_API/${project_name}/${CONTAINER_ACCOUNTS[$project_name]}/$account/run.sh"
             sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/$project_name/run.sh" "$GITHUB_API/${project_name}/${CONTAINER_ACCOUNTS[$project_name]}/$account/run.sh"
         fi
