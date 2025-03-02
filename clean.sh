c60cfb648966fe9fad1ba95388b3b7b21f3fe012
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-03 00:32:48 +0800

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,7 @@
+# 清理沒有 tag 的 images
+echo "清理未標記的 images..."
+sudo podman images -f "dangling=true" -q | xargs sudo podman rmi -f
+
+# 清理未使用的 images
+echo "清理未使用的 images..."
+sudo podman image prune -a -f
