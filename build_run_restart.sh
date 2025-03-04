4e5a4852bdf7ed26939517abda5f0fbaa78972c5
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-04 13:59:56 +0800

    Update build_run_restart.sh

--- build_run_restart.sh
+++ build_run_restart.sh
@@ -113,13 +113,3 @@ process_container "DistributeAi" "$UPDATE_DistributeAi" "distributeai" "accounts
 process_container "Nodego" "$UPDATE_Nodego" "nodego" "tokens.txt" "$SLEEP_TIME" "$ACTION"
 process_container "Sparkchain" "$UPDATE_Sparkchain" "sparkchain" "accounts.json" "$SLEEP_TIME" "$ACTION"
 
-
-
-
-# 清理沒有 tag 的 images
-echo "清理未標記的 images..."
-#sudo podman images -f "dangling=true" -q | xargs sudo podman rmi -f
-
-# 清理未使用的 images
-echo "清理未使用的 images..."
-#sudo podman image prune -a -f
