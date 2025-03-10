14593dcdc59e70ca746e5adaeaf9cf1da024cf5c
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-10 14:57:57 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -1,3 +1,3 @@
-sudo podman run -d --rm --restart always --replace -m 50m -v /opt/build_image/unich/tokens.txt:/opt/unich/tokens.txt --name Unich docker.io/78chicken/unich:latest
+sudo podman run -d --rm --replace -m 50m -v /opt/build_image/unich/tokens.txt:/opt/unich/tokens.txt --name Unich docker.io/78chicken/unich:latest
 sleep 30s 
 sudo podman stop Unich
