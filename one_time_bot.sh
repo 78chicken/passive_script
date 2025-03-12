b22ad257767e350aae14329c2cfec0cfd746f26f
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-12 10:22:22 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -2,7 +2,8 @@ BASE_DIR="/opt/build_image"
 GITHUB_REPO="78chicken/config"
 GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/unich/accounts.json" "$GITHUB_API/unich/all/accounts.json"
-sudo podman run -d --rm --replace -m 50m -v /opt/build_image/unich/tokens.txt:/opt/unich/tokens.txt --name Unich docker.io/78chicken/unich:latest
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/unich/tokens.txt" "$GITHUB_API/unich/all/tokens.txt"
+echo "download $GITHUB_API/unich/all/tokens.txt to ${BASE_DIR}/unich/tokens.txt"
+sudo podman run -d --rm --replace -m 40m -v ${BASE_DIR}/unich/tokens.txt:/opt/unich/tokens.txt --name Unich docker.io/78chicken/unich:latest
 sleep 30s 
 sudo podman stop Unich
