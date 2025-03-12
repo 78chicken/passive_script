c8004ed658624d33ce023ba97ce47eea544d42ab
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-13 00:13:52 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -8,8 +8,8 @@ sudo podman run -d --rm --replace -m 40m -v ${BASE_DIR}/unich/tokens.txt:/app/un
 sleep 30s 
 sudo podman stop Unich
 
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/hahawallet/jyhfengli/accounts.json" "$GITHUB_API/hahawallet/jyhfengli/accounts.json"
-echo "download $GITHUB_API/hahawallet/jyhfengli/accounts.json to ${BASE_DIR}/hahawallet/jyhfengli/accounts.json"
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/hahawallet/accounts.json" "$GITHUB_API/hahawallet/jyhfengli/accounts.json"
+echo "download $GITHUB_API/hahawallet/jyhfengli/accounts.json to ${BASE_DIR}/hahawallet/accounts.json"
 sudo podman run -d --rm --replace -m 40m -v ${BASE_DIR}/hahawallet/accounts.json:/app/hahawallet/accounts.json:Z --name HahaWallet docker.io/78chicken/hahawallet:latest
 sleep 30s 
 sudo podman stop HahaWallet
