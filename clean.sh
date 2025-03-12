5e484913192d23afab2c4614a5141b864e00e268
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-12 15:47:04 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,3 +5,5 @@ sudo podman images -f "dangling=true" -q | xargs sudo podman rmi -f
 # 清理未使用的 images
 echo "清理未使用的 images..."
 sudo podman image prune -a -f
+# 清理未使用的 volume
+sudo podman volume prune -f
