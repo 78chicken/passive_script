708d9d6658ae7e3bb21d11b675a008f12acca27e
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-19 10:25:55 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -1,15 +1,25 @@
 BASE_DIR="/opt/build_image"
+QUEST_DIR="${BASE_DIR}/quest"
 GITHUB_REPO="78chicken/config"
 GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/unich/tokens.txt" "$GITHUB_API/unich/all/tokens.txt"
-echo "download $GITHUB_API/unich/all/tokens.txt to ${BASE_DIR}/unich/tokens.txt"
+#Unich
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/unich/tokens.txt" "$GITHUB_API/unich/all/tokens.txt"
+echo "download $GITHUB_API/unich/all/tokens.txt to ${QUEST_DIR}/unich/tokens.txt"
 sudo podman run -d --rm --replace -m 40m -v ${BASE_DIR}/unich/tokens.txt:/app/unich/tokens.txt:Z --name Unich docker.io/78chicken/unich:latest
 sleep 30s 
 sudo podman stop Unich
 
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${BASE_DIR}/hahawallet/accounts.json" "$GITHUB_API/hahawallet/jyhfengli/accounts.json"
-echo "download $GITHUB_API/hahawallet/jyhfengli/accounts.json to ${BASE_DIR}/hahawallet/accounts.json"
-sudo podman run -d --rm --replace -m 40m -v ${BASE_DIR}/hahawallet/accounts.json:/app/hahawallet/accounts.json:Z --name HahaWallet docker.io/78chicken/hahawallet:latest
+＃HahaWallet
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/hahawallet/accounts.json" "$GITHUB_API/hahawallet/jyhfengli/accounts.json"
+echo "download $GITHUB_API/hahawallet/jyhfengli/accounts.json to ${QUEST_DIR}/hahawallet/accounts.json"
+sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/hahawallet/accounts.json:/app/hahawallet/accounts.json:Z --name HahaWallet docker.io/78chicken/hahawallet:latest
 sleep 30s 
 sudo podman stop HahaWallet
+
+＃DreamerQuests
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/dreamerquests/cookies.txt" "$GITHUB_API/dreamerquests/all/cookies.txt"
+echo "download $GITHUB_API/dreamerquests/all/cookies.txt to ${QUEST_DIR}/dreamerquests/cookies.txt"
+sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/dreamerquests/cookies.txt:/app/dreamerquests/cookies.txt:Z --name DreamerQuests docker.io/78chicken/dreamerquests:latest
+sleep 30s 
+sudo podman stop DreamerQuests
