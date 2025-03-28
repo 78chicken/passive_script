2d3aab570fe9a9a9e062feaf00b131db64e14282
78chicken <165230596+78chicken@users.noreply.github.com>
2025-03-29 00:14:14 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -3,6 +3,13 @@ QUEST_DIR="${BASE_DIR}/quest"
 GITHUB_REPO="78chicken/config"
 GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 
+#ByData
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/bydata/accounts.txt" "$GITHUB_API/bydata/all/accounts.txt"
+echo "download $GITHUB_API/bydata/all/accounts.txt to ${QUEST_DIR}/bydata/accounts.txt"
+sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/bydata/accounts.txt:/app/bydata/accounts.txt:Z --name ByData docker.io/78chicken/bydata:latest
+sleep 60s 
+sudo podman stop ByData
+
 #Unich
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/unich/tokens.txt" "$GITHUB_API/unich/all/tokens.txt"
 echo "download $GITHUB_API/unich/all/tokens.txt to ${QUEST_DIR}/unich/tokens.txt"
@@ -24,9 +31,4 @@ sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/dreamerquests/cookies.t
 sleep 30s 
 sudo podman stop DreamerQuests
 
-#ByData
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/bydata/accounts.txt" "$GITHUB_API/bydata/all/accounts.txt"
-echo "download $GITHUB_API/bydata/all/accounts.txt to ${QUEST_DIR}/bydata/accounts.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/bydata/accounts.txt:/app/bydata/accounts.txt:Z --name ByData docker.io/78chicken/bydata:latest
-sleep 60s 
-sudo podman stop ByData
+
