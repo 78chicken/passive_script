423d107b5dab30ead9686f98d31870b34df6c004
78chicken <165230596+78chicken@users.noreply.github.com>
2025-04-10 09:48:44 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -3,6 +3,13 @@ QUEST_DIR="${BASE_DIR}/quest"
 GITHUB_REPO="78chicken/config"
 GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 
+#Coresky
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/coresky/accounts.txt" "$GITHUB_API/coresky/all/accounts.txt"
+echo "download $GITHUB_API/coresky/all/accounts.txt to ${QUEST_DIR}/coresky/accounts.txt"
+sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/coresky/accounts.txt:/app/coresky/accounts.txt:Z --name Coresky docker.io/78chicken/coresky:latest
+sleep 60s 
+sudo podman stop Coresky
+
 #TakerProtocol
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/takerprotocol/accounts.txt" "$GITHUB_API/takerprotocol/all/accounts.txt"
 echo "download $GITHUB_API/takerprotocol/all/accounts.txt to ${QUEST_DIR}/takerprotocol/accounts.txt"
@@ -11,11 +18,11 @@ sleep 60s
 sudo podman stop TakerProtocol
 
 #MonadScore
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/monadscore/query.txt" "$GITHUB_API/monadscore/all/query.txt"
-echo "download $GITHUB_API/monadscore/all/query.txt to ${QUEST_DIR}/monadscore/query.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/monadscore/query.txt:/app/monadscore/query.txt:Z --name MonadScore docker.io/78chicken/monadscore:latest
-sleep 180s 
-sudo podman stop MonadScore
+#sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/monadscore/query.txt" "$GITHUB_API/monadscore/all/query.txt"
+#echo "download $GITHUB_API/monadscore/all/query.txt to ${QUEST_DIR}/monadscore/query.txt"
+#sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/monadscore/query.txt:/app/monadscore/query.txt:Z --name MonadScore docker.io/78chicken/monadscore:latest
+#sleep 180s 
+#sudo podman stop MonadScore
 
 #ByData
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/bydata/accounts.txt" "$GITHUB_API/bydata/all/accounts.txt"
