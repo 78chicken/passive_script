27cfc5b967a8147b68922f853cc2accd15b63a31
78chicken <165230596+78chicken@users.noreply.github.com>
2025-04-14 14:55:52 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -6,63 +6,63 @@ GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 #kivanet
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/kivanet/accounts.json" "$GITHUB_API/kivanet/all/accounts.json"
 echo "download $GITHUB_API/kivanet/all/accounts.json to ${QUEST_DIR}/kivanet/accounts.json"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/kivanet/accounts.json:/app/kivanet/accounts.json:Z --name Kivanet docker.io/78chicken/kivanet:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/kivanet/accounts.json:/app/kivanet/accounts.json:Z --name Kivanet docker.io/78chicken/kivanet:latest
 sleep 60s 
 sudo podman stop Kivanet
 
 #zeroswallet
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/zeroswallet/accounts.txt" "$GITHUB_API/zeroswallet/all/accounts.txt"
 echo "download $GITHUB_API/zeroswallet/all/accounts.txt to ${QUEST_DIR}/zeroswallet/accounts.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/zeroswallet/accounts.txt:/app/zeroswallet/accounts.txt:Z --name ZerosWallet docker.io/78chicken/zeroswallet:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/zeroswallet/accounts.txt:/app/zeroswallet/accounts.txt:Z --name ZerosWallet docker.io/78chicken/zeroswallet:latest
 sleep 120s 
 sudo podman stop ZerosWallet
 
 #Coresky
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/coresky/accounts.txt" "$GITHUB_API/coresky/all/accounts.txt"
 echo "download $GITHUB_API/coresky/all/accounts.txt to ${QUEST_DIR}/coresky/accounts.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/coresky/accounts.txt:/app/coresky/accounts.txt:Z --name Coresky docker.io/78chicken/coresky:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/coresky/accounts.txt:/app/coresky/accounts.txt:Z --name Coresky docker.io/78chicken/coresky:latest
 sleep 60s 
 sudo podman stop Coresky
 
 #TakerProtocol
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/takerprotocol/accounts.txt" "$GITHUB_API/takerprotocol/all/accounts.txt"
 echo "download $GITHUB_API/takerprotocol/all/accounts.txt to ${QUEST_DIR}/takerprotocol/accounts.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/takerprotocol/accounts.txt:/app/takerprotocol/accounts.txt:Z --name TakerProtocol docker.io/78chicken/takerprotocol:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/takerprotocol/accounts.txt:/app/takerprotocol/accounts.txt:Z --name TakerProtocol docker.io/78chicken/takerprotocol:latest
 sleep 60s 
 sudo podman stop TakerProtocol
 
 #MonadScore
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/monadscore/query.txt" "$GITHUB_API/monadscore/all/query.txt"
 echo "download $GITHUB_API/monadscore/all/query.txt to ${QUEST_DIR}/monadscore/query.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/monadscore/query.txt:/app/monadscore/query.txt:Z --name MonadScore docker.io/78chicken/monadscore:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/monadscore/query.txt:/app/monadscore/query.txt:Z --name MonadScore docker.io/78chicken/monadscore:latest
 sleep 180s 
 sudo podman stop MonadScore
 
 #ByData
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/bydata/accounts.txt" "$GITHUB_API/bydata/all/accounts.txt"
 echo "download $GITHUB_API/bydata/all/accounts.txt to ${QUEST_DIR}/bydata/accounts.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/bydata/accounts.txt:/app/bydata/accounts.txt:Z --name ByData docker.io/78chicken/bydata:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/bydata/accounts.txt:/app/bydata/accounts.txt:Z --name ByData docker.io/78chicken/bydata:latest
 sleep 300s 
 sudo podman stop ByData
 
 #Unich
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/unich/tokens.txt" "$GITHUB_API/unich/all/tokens.txt"
 echo "download $GITHUB_API/unich/all/tokens.txt to ${QUEST_DIR}/unich/tokens.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/unich/tokens.txt:/app/unich/tokens.txt:Z --name Unich docker.io/78chicken/unich:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/unich/tokens.txt:/app/unich/tokens.txt:Z --name Unich docker.io/78chicken/unich:latest
 sleep 30s 
 sudo podman stop Unich
 
 #HahaWallet
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/hahawallet/accounts.json" "$GITHUB_API/hahawallet/jyhfengli/accounts.json"
 echo "download $GITHUB_API/hahawallet/jyhfengli/accounts.json to ${QUEST_DIR}/hahawallet/accounts.json"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/hahawallet/accounts.json:/app/hahawallet/accounts.json:Z --name HahaWallet docker.io/78chicken/hahawallet:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/hahawallet/accounts.json:/app/hahawallet/accounts.json:Z --name HahaWallet docker.io/78chicken/hahawallet:latest
 sleep 30s 
 sudo podman stop HahaWallet
 
 #DreamerQuests
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/dreamerquests/cookies.txt" "$GITHUB_API/dreamerquests/all/cookies.txt"
 echo "download $GITHUB_API/dreamerquests/all/cookies.txt to ${QUEST_DIR}/dreamerquests/cookies.txt"
-sudo podman run -d --rm --replace -m 40m -v ${QUEST_DIR}/dreamerquests/cookies.txt:/app/dreamerquests/cookies.txt:Z --name DreamerQuests docker.io/78chicken/dreamerquests:latest
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/dreamerquests/cookies.txt:/app/dreamerquests/cookies.txt:Z --name DreamerQuests docker.io/78chicken/dreamerquests:latest
 sleep 30s 
 sudo podman stop DreamerQuests
 
