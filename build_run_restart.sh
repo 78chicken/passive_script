6f22e0ca67dcb3bb52a539da2454e734ab1de943
78chicken <165230596+78chicken@users.noreply.github.com>
2025-04-16 23:27:56 +0800

    Update build_run_restart.sh

--- build_run_restart.sh
+++ build_run_restart.sh
@@ -65,7 +65,7 @@ UPDATE_OptimAi="Y"
 UPDATE_ByData="Y"
 UPDATE_OpenLedger="Y"
 UPDATE_Mygate="Y"
-UPDATE_Meganet="Y"
+UPDATE_Meganet="N"
 UPDATE_Flow3="Y"
 
 #以下目前無法賺錢的
@@ -120,7 +120,7 @@ process_container() {
 
 # 依據設定執行不同的更新方式
 process_container "Mygate" "$UPDATE_Mygate" "mygate" "tokens.txt" "$SLEEP_TIME" "$ACTION"
-process_container "Meganet" "$UPDATE_Meganet" "meganet" "accounts.txt" "$SLEEP_TIME" "$ACTION"
+process_container "Meganet" "$UPDATE_Meganet" "meganet" "accounts.txt" "$SLEEP_TIME" "STOP"
 process_container "Exeos" "$UPDATE_Exeos" "exeos" "accounts.json" "$SLEEP_TIME" "STOP"
 process_container "OptimAi" "$UPDATE_OptimAi" "optimai" "accounts.json" "$SLEEP_TIME" "$ACTION"
 process_container "OpenLedger" "$UPDATE_OpenLedger" "openledger" "accounts.json" "$SLEEP_TIME" "$ACTION"
