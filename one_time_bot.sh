533368a2226d257f9ee6106d6327bddf198221a9
78chicken <165230596+78chicken@users.noreply.github.com>
2025-04-17 17:30:30 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -7,7 +7,7 @@ GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/unich/tokens.txt" "$GITHUB_API/unich/all/tokens.txt"
 echo "download $GITHUB_API/unich/all/tokens.txt to ${QUEST_DIR}/unich/tokens.txt"
 sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/unich/tokens.txt:/app/unich/tokens.txt:Z --name Unich docker.io/78chicken/unich:latest
-sleep 30s 
+sleep 60s 
 sudo podman stop Unich
 
 #PuzzleMania
