d9ccc0833ccc12a8139b2675076591d49604cf41
78chicken <165230596+78chicken@users.noreply.github.com>
2025-05-10 11:06:14 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -32,11 +32,11 @@ sleep 60s
 sudo podman stop TakerProtocol 
 
 #MonadScore
-sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/monadscore/query.txt" "$GITHUB_API/monadscore/all/query.txt"
-echo "download $GITHUB_API/monadscore/all/query.txt to ${QUEST_DIR}/monadscore/query.txt"
-sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/monadscore/query.txt:/app/monadscore/query.txt:Z --name MonadScore docker.io/78chicken/monadscore:latest
-sleep 180s 
-sudo podman stop MonadScore
+#sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/monadscore/query.txt" "$GITHUB_API/monadscore/all/query.txt"
+#echo "download $GITHUB_API/monadscore/all/query.txt to ${QUEST_DIR}/monadscore/query.txt"
+#sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/monadscore/query.txt:/app/monadscore/query.txt:Z --name MonadScore docker.io/78chicken/monadscore:latest
+#sleep 180s 
+#sudo podman stop MonadScore
 
 #GpuNet
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/gpunet/accounts.txt" "$GITHUB_API/gpunet/all/accounts.txt"
