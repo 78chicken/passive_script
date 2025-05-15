a2eab11f57752edeec8b88d1e19d089d06e5c200
78chicken <165230596+78chicken@users.noreply.github.com>
2025-05-15 11:47:14 +0800

    Update one_time_bot.sh

--- one_time_bot.sh
+++ one_time_bot.sh
@@ -3,6 +3,13 @@ QUEST_DIR="${BASE_DIR}/quest"
 GITHUB_REPO="78chicken/config"
 GITHUB_API="https://api.github.com/repos/${GITHUB_REPO}/contents"
 
+#Billions
+sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/billions/cookies.txt" "$GITHUB_API/billions/all/cookies.txt"
+echo "download $GITHUB_API/billions/all/accounts.txt to ${QUEST_DIR}/billions/accounts.txt"
+sudo podman run -d --rm --replace -m 50m -v ${QUEST_DIR}/billions/cookies.txt:/app/billions/cookies.txt:Z --name Billions docker.io/78chicken/billions:latest
+sleep 60s 
+sudo podman stop Billions
+
 #openverse
 sudo curl -s -H "Accept: application/vnd.github.v3.raw" -H "Authorization: token ${GITHUB_TOKEN}" -o "${QUEST_DIR}/openverse/accounts.txt" "$GITHUB_API/openverse/jyhfengli/accounts.txt"
 echo "download $GITHUB_API/openverse/all/accounts.txt to ${QUEST_DIR}/openverse/accounts.txt"
